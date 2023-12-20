6f5d94b4bde10b5e64edff3ef11e9aa4ed4afce1
Nde Dilane <nde.dilan@ictuniversity.edu.cm>
2023-12-20 20:15:06 +0100

    Setting sanity

---
+++ ecommerce/schemas/product.js
@@ -0,0 +1,42 @@
+export default {
+    name:'product',
+    title:'Product',
+    type:'document',
+    fields:[
+        {
+            name:'image',
+            title:'Image',
+            type:'array',
+            of:[{type:'image'}],
+            Option:{
+                hotspot:true
+            }
+        },
+        {
+            name:'name',
+            title:'Name',
+            type:'string',
+        },
+        {
+            name:'slug',
+            title:'Slug',
+            type:'slug',
+            options:{
+                source:'name',
+                maxLength:100,
+            }
+        },
+        {
+            name:'description',
+            title:'Description',
+            type:'text',
+        },
+        {
+            name:'price',
+            title:'Price',
+            type:'number',
+        },
+       
+    ],
+    
+}
